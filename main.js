2a67128b6b7b899c79ea4a9883a7d84fd214e786
spaynkee <spaynkee@gmail.com>
2019-06-29 21:13:30 -0500

    finished progress bar for now.

--- main.js
+++ main.js
@@ -4,10 +4,11 @@ const express = require('express'),
 	app = express(),
 	request = require('request'),
 	path = require('path'),
-	upload = require('./resources/js/upload.js');
-	//bootstrap = require('bootstrap')
-	//jquery = require('jquery');
-
+	upload = require('./resources/js/upload.js'),
+	formidable = require('formidable'),
+	fs = require('fs');
+	
+    
 app.use('/public', express.static(path.join(__dirname + '/resources/css')));
 app.use('/public', express.static(path.join(__dirname + '/resources/js')));
 app.set(express.static(path.join(__dirname + './views')));
@@ -17,29 +18,10 @@ var process_spawner = require('child_process');
 
 //this works as a global var for progress bar.
 app.locals.progress = "0";
-	//path = require('path'),
-	//upload = require('./upload.js');
-	//bootstrap = require('bootstrap')
-	//jquery = require('jquery');
 
 app.use(express.static(path.join(__dirname + '/resources/css')));
 app.set(express.static(path.join(__dirname + 'views')));
 app.set('view engine', 'ejs');
-
-//app.set('views', 'views');
-
-// app.get('/', function(req, res) {
-// 	request(
-// 		{
-// 			url: 'http://www.colourlovers.com/api/colors/random?format=json',
-// 			json: true
-// 		},
-// 		function(err, response, body) {
-// 			res.sendFile('layout.html', {root: __dirname + '/views/'}); //{title: body[0].title, hex: body[0].hex, dateCreated: body[0].dateCreated}
-// 		},
-
-// 	);
-// });
 
 app.get('/', (req, res) => {
 	res.render('layout', {root: __dirname + '/views/'});
@@ -58,42 +40,83 @@ app.get('/Contact', (req, res) => {
 })
 app.get('/Popup', (req, res) => {
 	res.render('popup', {root: __dirname + '/views/'});
-})
+});
 
 app.post('/submit-form', (req, res) => {
-	res.render('Upload');
+	res.render('Upload', {root: __dirname + '/views/'});
+	var form = new formidable.IncomingForm();
 
-	//This runs test.py, which increments and outputs a number every 1 seconds.
-	var PRIS = process_spawner.spawn('python', ['resources'+'\\test.py']);
+	form.multiples=true;
+	form.parse(req);
 
-	//Captures output for as long as the python file is running, and updates the global var.
-	PRIS.stdout.on('data', (data) => {
-		console.log(data.toString());
-		req.app.locals.progress = data.toString();
-	});
+	form.on('file',function(name, file)
+	{
+		var fName = "", len = Math.random()*6+8;
+
+		for(var i = 0; i < len; i++){
+			fName+=Math.round(Math.random()*9);
+		}
+
+		var type = file.type.substring(0,5);
+		if(type=='video'){
+			var ffmpeg = process_spawner.spawn('resources/ffmpeg', ['-i',file.path,'-filter:v','fps=fps=5', './resources/upload_tmp/'+fName+'.avi']);
+			ffmpeg.on('close',function(e){
+				
+				fs.unlink(file.path, (err) => {
+					if (err) throw err;
+					console.log(file.path+" deleted successfully");
+				});
+
+				var PRIS = process_spawner.spawn('python', ['resources\\PRIS\\core.py', process.cwd()+"\\resources\\upload_tmp\\"+fName+".avi"]);
+				//Captures output for as long as the python file is running, and updates the global var.
+				PRIS.stdout.on('data', (data) => {
+					//if it's a frameDone message.
+					console.log(data.toString());
+
+					if(data.toString().indexOf("PD:") != -1)
+					{
+						//updates the global progress var.
+						req.app.locals.progress = data.toString().substring(data.toString().indexOf("PD:")+3, data.toString().indexOf("%"));
+					}
+				});
+		});
+	}
+	else if(type=='image'){
+		var ffmpeg = process_spawner.spawn('resources/ffmpeg', ['-loop','1','-i',file.path,'-r','1','-t','1','-vcodec','libx264','./resources/upload_tmp/'+fName+'.avi']);
+		ffmpeg.on('close',function(e){
+			fs.unlink(file.path, (err) => {
+				if (err) throw err;
+				console.log(file.path+" deleted successfully");
+			});
+			var PRIS = process_spawner.spawn('python', ['resources\\PRIS\\core.py', process.cwd()+"\\resources\\upload_tmp\\"+fName+".avi"]);
+
+				//Captures output for as long as the python file is running, and updates the global var.
+				PRIS.stdout.on('data', (data) => {
+					//if it's a frameDone message.
+					console.log(data.toString());
+
+					if(data.toString().indexOf("PD:") != -1)
+					{
+						//updates the global progress var.
+						req.app.locals.progress = data.toString().substring(data.toString().indexOf("PD:")+3, data.toString().indexOf("%"));
+					}
+				});
+		});
+	}
+	else{
+		//pull this out to a function maybe?
+		fs.unlink(file.path, (err) => {
+			if (err) throw err;
+			console.log(file.path+" deleted successfully");
+		});
+	} 
+});
 });
 
 //pulls back the global var with ajax.
 app.get('/getprogress', (req, res) => {
 	res.json({prog: req.app.locals.progress})
 });
-
-//Backend stuff
-app.use(upload.uploadAndConvert);
-
-app.post('/fileupload', function(req, res){
-	return res.end();
-});
-
-app.get('/uploadtest', function (req, res){
-			res.writeHead(200, {'Content-type': 'text/html'});
-			res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-			res.write('<input type="file" name="input" multiple="multiple"><br>');
-			res.write('<input type="submit">');
-			res.write('</form>');
-			return res.end();
-});
-// End backend stuff
 
 const server = app.listen(3000, function() {
 	console.log(`Server started on port ${server.address().port}`);
