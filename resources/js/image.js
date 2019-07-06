b1c52a13ca08dba9522aa37e52c8f710f9a7c107
Micayla King <king110@live.missouristate.edu>
2019-07-06 15:46:25 -0500

    image.js created

---
+++ resources/js/image.js
@@ -0,0 +1,15 @@
+function LoadImg(filename) {
+    var xmlhttp;
+    if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    } else { // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {     
+            document.getElementById("documentHolder").src = "data:image/png;base64," + xmlhttp.responseText;
+        }
+    };    
+    xmlhttp.open("GET", 'load.php?LoadImg='+filename );
+    xmlhttp.send(null);
+}
