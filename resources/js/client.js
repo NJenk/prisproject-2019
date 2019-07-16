3da841c8fe5404ef9503fae3d07c53f14eb92b8d
Unknown <sd0854388@otc.edu>
2019-07-16 17:05:56 -0500

    progress bars

--- resources/js/client.js
+++ resources/js/client.js
@@ -41,6 +41,18 @@ function getProgress()
                                 //it did exist, so just update it.
                                 var percent_done = Math.ceil(((upload.current_frames/upload.total_frames) * 100)+1);
                                 existing_progress_bar.style = "width: "+percent_done+"%";
+                                if(percent_done < 33){
+                                    existing_progress_bar.classList.add('progress-bar-1');
+                                }
+                                else if(percent_done >= 33 && percent_done < 67){
+                                    existing_progress_bar.classList.add('progress-bar-33');
+                                }
+                                else if(percent_done >= 67 && percent_done < 100){
+                                    existing_progress_bar.classList.add('progress-bar-67');
+                                }
+                                else if(percent_done == 100){
+                                    existing_progress_bar.classList.add('progress-bar');                                
+                                }
 
                             }
                             else
@@ -60,19 +72,19 @@ function getProgress()
 
 function createProgressBar(upload)
 {
-    var prog_bar = document.createElement("div")
+    var progress_bar = document.createElement("div")
 
-    prog_bar.classList.add('progress-bar');
-    prog_bar.classList.add('progress-bar-striped');
-    prog_bar.classList.add('progress-bar-animated');
-    prog_bar.setAttribute('role', 'progressbar');
-    prog_bar.setAttribute('aria-valuenow', '10');
-    prog_bar.setAttribute('aria-valuemin', '0');
-    prog_bar.setAttribute('aria-valuemax', '100');
+    progress_bar.classList.add('progress-bar');
+    progress_bar.classList.add('progress-bar-striped');
+    progress_bar.classList.add('progress-bar-animated');
+    progress_bar.setAttribute('role', 'progressbar');
+    progress_bar.setAttribute('aria-valuenow', '10');
+    progress_bar.setAttribute('aria-valuemin', '0');
+    progress_bar.setAttribute('aria-valuemax', '100');
 
-    prog_bar.id = "pb"+upload.temp_name;
-    prog_bar.style = "width: 0%";
-    prog_bar.innerHTML = upload.original_name;
+    progress_bar.id = "pb"+upload.temp_name;
+    progress_bar.style = "width: 0%";
+    progress_bar.innerHTML = upload.original_name;
 
-    return prog_bar;
+    return progress_bar;
 }
