5d7fc1f64a419442dd5f6eccddd8b8ddcde6a2fe
spaynkee <spaynkee@gmail.com>
2019-07-17 08:09:07 -0500

    added X to upload bars

--- resources/js/client.js
+++ resources/js/client.js
@@ -34,6 +34,17 @@ function getProgress()
                             inner_div.classList.add('progress');
 
                             var existing_progress_bar = document.querySelector('#pb'+upload.temp_name)
+
+                            if(upload.current_frames+1 === upload.total_frames)
+                            {
+                                if(existing_progress_bar)
+                                {
+                                    existing_progress_bar.style = "width: 100%";
+                                }
+                                
+                                return;
+                            }
+
                             //For each progress in this array, make a progress bar.
                             //If the progress bar doesn't exist, create and append it. if it does exist, just update the current progress.
                             if(existing_progress_bar)
@@ -41,16 +52,17 @@ function getProgress()
                                 //it did exist, so just update it.
                                 var percent_done = Math.ceil(((upload.current_frames/upload.total_frames) * 100)+1);
                                 existing_progress_bar.style = "width: "+percent_done+"%";
-
                             }
                             else
                             {
 
+                                var new_alert = createAlert(upload.original_name);
+
                                 var prog_bar = createProgressBar(upload);
-
-                                outer_div.appendChild(inner_div);
                                 inner_div.appendChild(prog_bar);
-
+                                new_alert.appendChild(inner_div);
+                                outer_div.appendChild(new_alert);
+                                console.log("Should be appended...");
                             }
                         });
                     }
@@ -75,4 +87,29 @@ function createProgressBar(upload)
     prog_bar.innerHTML = upload.original_name;
 
     return prog_bar;
+}
+
+function createAlert(fileName)
+{
+    var alert_base_div = document.createElement('div');
+    alert_base_div.classList.add("alert");
+    alert_base_div.classList.add("alert-dismissible");
+    alert_base_div.classList.add("bg-secondary");
+
+    var alert_heading = document.createElement('h4');
+    alert_heading.classList.add('alert-heading');
+    alert_heading.innerHTML = fileName;
+
+    var alert_close_button = document.createElement('button');
+    alert_close_button.setAttribute('type', 'button');
+    alert_close_button.classList.add("close");
+    alert_close_button.setAttribute('data-dismiss', 'alert');
+    alert_close_button.innerHTML = '×';
+    //Append an onclick function that sets 
+
+    alert_base_div.appendChild(alert_close_button);
+    alert_base_div.appendChild(alert_heading);
+
+    console.log("There should be an alert here lul");
+    return alert_base_div;
 }
